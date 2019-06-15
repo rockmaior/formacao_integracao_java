1df79d7ed314a98ed50bb53773f3a62905163029
Regis Ribeiro <rockmaior@gmail.com>
2019-06-15 01:06:23 -0300

    Aula 03 - Trabalhando com parametros

--- src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
+++ src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
@@ -2,7 +2,9 @@ package br.com.alura.loja.resource;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import br.com.alura.loja.dao.CarrinhoDAO;
@@ -16,9 +18,11 @@ public class CarrinhoResource {
 	 * Produz uma resposta de conteudo em XML requisitado via GET para buscar informacoes do servidor.
 	 */
 	
+	
+	@Path("{id}")
 	@GET
 	@Produces(MediaType.APPLICATION_XML)
-	public String busca() {
+	public String busca(@PathParam("id") long id) {
 		Carrinho carrinho = new CarrinhoDAO().busca(1l);
 		return carrinho.toXML();
 	}
