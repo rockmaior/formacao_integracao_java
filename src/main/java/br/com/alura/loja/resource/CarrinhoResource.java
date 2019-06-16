b0337a60641ecbc9281429ee3db08fd8fca7a7bd
Regis Ribeiro <rockmaior@gmail.com>
2019-06-16 07:22:24 -0300

    Revert "Aula 03 - Trabalhando com parametros"
    This reverts commit 1df79d7ed314a98ed50bb53773f3a62905163029.

--- src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
+++ src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
@@ -2,9 +2,7 @@ package br.com.alura.loja.resource;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import br.com.alura.loja.dao.CarrinhoDAO;
@@ -18,11 +16,9 @@ public class CarrinhoResource {
 	 * Produz uma resposta de conteudo em XML requisitado via GET para buscar informacoes do servidor.
 	 */
 	
-	
-	@Path("{id}")
 	@GET
 	@Produces(MediaType.APPLICATION_XML)
-	public String busca(@PathParam("id") long id) {
+	public String busca() {
 		Carrinho carrinho = new CarrinhoDAO().busca(1l);
 		return carrinho.toXML();
 	}
