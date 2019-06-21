47b6fca4bf983c29b2525fec7be99d960b863012
Regis Ribeiro <rockmaior@gmail.com>
2019-06-21 12:18:13 -0300

    Aula 11 atualizando recurso com PUT. Parte 1

--- src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
+++ src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
@@ -6,6 +6,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -16,6 +17,7 @@ import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.loja.dao.CarrinhoDAO;
 import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Produto;
 
 @Path("carrinhos")
 public class CarrinhoResource {
@@ -55,5 +57,29 @@ public class CarrinhoResource {
 		carrinho.remove(produtoId);
 		return Response.ok().build();
 	}
+	
+	/**path anterior
+	put para alterar um recurso
+	alteraproduto passando os mesmo parametros anteriores
+	Busca o carrionho com o id
+	Receber o conteudo XML do Produto no parametro do metodo
+	transoformar o conteudo do produto fromxml
+	pegar o carrinho e chamar o metodo troca para trocar produto
+	testar com o curl
+	
+	alterar a URI do path acrecentando a quantidade
+	altera carrinho para metodo trocaQuantidade que vai so trocar a quantidade do produto.
+	*/
+	
+	@Path("{id}/produtos/{produtoId}")
+	@PUT
+	@Consumes
+	public Response alteraProduto(String conteudo, @PathParam("id") long id, @PathParam("produtoId") long produtoId) {
+		Carrinho carrinho = new CarrinhoDAO().busca(id);
+		Produto produto = (Produto) new XStream().fromXML(conteudo);
+		carrinho.troca(produto );
+		return Response.ok().build();
+	}
 
 }
+
--- src/test/java/br/com/alura/loja/ClienteTest.java
+++ src/test/java/br/com/alura/loja/ClienteTest.java
@@ -8,6 +8,8 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.filter.LoggingFilter;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -19,51 +21,55 @@ import br.com.alura.loja.modelo.Carrinho;
 import br.com.alura.loja.modelo.Produto;
 
 public class ClienteTest {
-	
+
 	private HttpServer server;
-	
+	private Client client;
+	private WebTarget target;
+
 	@Before
 	public void startaServidor() {
 		this.server = Servidor.inicializaServidor();
+		ClientConfig config = new ClientConfig();
+		config.register(new LoggingFilter());
+		this.client = ClientBuilder.newClient(config);// Cria um cliente HTTP para acessar o servidor
+		this.target = client.target("http://localhost:8080"); // URI base do servidor alvo das requisicoes.
+
 	}
-	
+
 	@After
 	public void mataServidor() {
-		
+
 		server.stop();
 	}
 
 	@Test
 	public void testaQueBuscarUmCarrinhoTrazOCarrinhoEsperado() {
-		
-		Client client = ClientBuilder.newClient();//Cria um cliente HTTP para acessar o servidor
-		WebTarget target = client.target("http://localhost:8080"); //URI base do servidor alvo das requisicoes.
-		//URI especifica do endpoint, ou path, que pega os dados do servidor e devolve a resposta em string.
+
+		// URI especifica do endpoint, ou path, que pega os dados do servidor e devolve
+		// a resposta em string.
 		String conteudo = target.path("/carrinhos/1").request().get(String.class);
-		
-		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);//desserealizar o XML para objeto
-		
+		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);// desserealizar o XML para objeto
 		Assert.assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
 
 	}
-	
+
 	@Test
 	public void testaQueSuportaNovosCarrinhos() {
 		Client client = ClientBuilder.newClient();
 		WebTarget target = client.target("http://localhost:8080");
-		
+
 		Carrinho carrinho = new Carrinho();
 		carrinho.adiciona(new Produto(314L, "Tablet", 999, 1));
-		carrinho.setRua( "Rua Vergueiro");
+		carrinho.setRua("Rua Vergueiro");
 		carrinho.setCidade("São Paulo");
 		String xml = carrinho.toXML();
-		
+
 		/**
 		 * Cria a representacao do recurso: o conteudo e o media type
 		 */
 		Entity<String> entity = Entity.entity(xml, MediaType.APPLICATION_XML);
 		Response response = target.path("/carrinhos").request().post(entity);
-		
+
 		Assert.assertEquals(201, response.getStatus());
 	}
 
